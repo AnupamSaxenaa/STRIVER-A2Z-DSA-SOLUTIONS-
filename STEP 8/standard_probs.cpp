ef54751797b2876c9144c11ec49f27d1983de978
Anupam Saxena <saxenaanupam2004@gmail.com>
2025-11-20 17:11:56 +0530

    ok

--- STEP 8/standard_probs.cpp
+++ STEP 8/standard_probs.cpp
@@ -2,45 +2,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//swap two nos
-void swappy(int a , int b)
+// swap two nos
+void swappy(int a, int b)
 {
-    a = a^b;
-    b = a^b;
-    a = a^b;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
 
-    cout<<a<<" "<<b<<endl;
+    cout << a << " " << b << endl;
     return;
 }
 
-//ith bit set or not
-void checkpls(int n,int i)
+// ith bit set or not
+void checkpls(int n, int i)
 {
-    n = n>>(i);
-    n = n&1;
-    if(n==0) cout<<"THE ith bit is not set\n";
-    else cout<<"THE ith bit is set\n";
+    n = n >> (i);
+    n = n & 1;
+    if (n == 0)
+        cout << "THE ith bit is not set\n";
+    else
+        cout << "THE ith bit is set\n";
     return;
 
-    //or do either way
-
-
+    // or do either way
 
     // if(((n)&(1<<i))!=0) cout<<"SET"<<endl;
     // else cout<<"NOT SET"<<endl;
-    
 }
 
-//set the ith bit
+// set the ith bit
 void setpls(int n, int i)
 {
-    n = n | (1<<i);
-    cout<<n<<endl;
+    n = n | (1 << i);
+    cout << n << endl;
     return;
 }
 
-int main() {
+// clear the ith bit
+void clearpls(int n, int i)
+{
+    cout << (n & ~(1 << i)) << endl;
+}
+
+// toggle the ith bit
+void togglepls(int n, int i)
+{
+    cout << (n ^ (1 << i)) << endl;
+}
+
+// remove the last set bit
+void removepls(int n)
+{
+    // int t = 1 ;
+    // while (t < n)
+    // {
+    //     int temp = (n & ~t);
+    //     if(temp!=n){
+    //         cout<<temp<<endl;
+    //         return;
+    //     }
+    //     t = t<<1;
+    // }
+
+    //or
+
+    cout<<(n & (n-1))<<endl;
+}
+
+int main()
+{
     cout << "PROGRAM RUNNING FINE!" << endl;
-    setpls(9,2);
+    removepls(0);
     return 0;
 }
